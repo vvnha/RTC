c1eec622fe9cc0788125ec03e564811a07b7b599
Võ Văn Nhã <38065969+vvnha@users.noreply.github.com>
2021-08-27 14:31:20 +0700

    Update sv.js

--- sv.js
+++ sv.js
@@ -66,4 +66,4 @@ io.on('connection', socket => {
     })
 })
 
-server.listen(process.env.PORT || 5000, () => console.log('server is running on port 5000'));
+server.listen(process.env.PORT || 5000, () => console.log(`server is running on port 5000 or ${process.env.PORT}`));
